b73656447d459c1c4ff90ae968bbaf40f685b5b0
Bruno Sampaio <brunoctsb@gmail.com>
2020-09-28 10:56:01 -0300

    Learning functions

---
+++ functions.js
@@ -0,0 +1,38 @@
+// basic structure of function
+function name(parameters) {execution}
+
+// other ways to declare a function:
+// Storing into a const/var/let ex.:
+const fun1 = function (parameters) {execution}
+
+// in a array ex.:
+const array = [function(parameters) {execution}, fun1, name]
+console.log(array[0](2, 3))
+
+// in a object ex.:
+const obj = {}
+obj.speak = function() {return 'Hello!'}
+console.log(obj.speak())
+
+//
+// Default value of a function
+
+function soma(a = 1, b = 1, c = 1){
+    return a + b + c
+}
+
+console.log(soma(0, 0, 0))
+
+// Arrow Funciton
+
+let double = (a) =>  2 * a
+
+let double = (a) =>  {
+    return 2 * a
+}
+
+let hello = function (){
+    return "Helllo!"
+}
+
+let hello = () => "Hello!"
